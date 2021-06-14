b59ece103282d1e43995301d9aa41f090d859e3e
Ahsan Tahseen <58149036+ahsantahseen@users.noreply.github.com>
2021-06-15 02:31:45 +0500

    Version 1.0

--- src/Components/Header/NavBar.js
+++ src/Components/Header/NavBar.js
@@ -7,22 +7,15 @@ const NavBar = () => {
     <>
       <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
         <Container className="mt-2 mb-2">
-          <Navbar.Brand>Logistics.pk</Navbar.Brand>
-          <Navbar.Toggle aria-controls="responsive-navbar-nav"></Navbar.Toggle>
-          <Navbar.Collapse>
-            <Nav className="me-auto justify-content-end">
-              <Nav.Link>Home</Nav.Link>
-              <Nav.Link>About</Nav.Link>
-              <Nav.Link>Order</Nav.Link>
-              <Nav.Link>Package Status</Nav.Link>
-            </Nav>
-            <Nav>
-              <Navbar.Text>
-                <FaBox style={{ marginRight: "10px", marginBottom: "2px" }} />
-                Delivering with care since 1974
-              </Navbar.Text>
-            </Nav>
-          </Navbar.Collapse>
+          <Navbar.Brand>
+            <h3>Logistics.pk</h3>
+          </Navbar.Brand>
+          <Navbar.Text>
+            <strong>
+              <FaBox style={{ marginRight: "10px", marginBottom: "2px" }} />
+              Delivering with care since 1974
+            </strong>
+          </Navbar.Text>
         </Container>
       </Navbar>
     </>
