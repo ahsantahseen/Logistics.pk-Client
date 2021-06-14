79224555a06a63a06e57d89863d61fc9e0ffdf9a
Ahsan Tahseen <58149036+ahsantahseen@users.noreply.github.com>
2021-06-15 02:51:16 +0500

    Version 1.0

--- src/Components/About Us/aboutUs.js
+++ src/Components/About Us/aboutUs.js
@@ -24,20 +24,20 @@ const AboutUs = () => {
         <Col md={6} className="bg-dark text-light aboutus">
           <h1 className="heading">About us</h1>
           <p>
-            TCS is a Pakistani courier and logistics company which is based in
-            Karachi, Pakistan. It was founded by a former Pakistan International
-            Airlines flight engineer, Khalid Nawaz Awan, in 1983. The company
-            also operates an e-commerce business Yayvo, previously known as TCS
-            Connect
+            Logistics.pk is a Pakistani courier and logistics company which is
+            based in Karachi, Pakistan. It was founded by a undergradute student
+            at SZABIST, Muhammad Ahsan, in 2020. The company was pitched as his
+            own startup, previously known as "Where is my parcel?" that started
+            as a tracking app. Now Logistics.pk is a full fleged company.
           </p>
           <p>
-            TCS maintains several thousand locations within Pakistan and
-            worldwide as well as its own airline operations, delivery vehicles
-            and couriers. TCS has been assigned machine readable passports
-            (MRPs) delivery services by the government,[4][5][6] and Visa
-            application submission and delivery services by embassies. TCS also
-            maintains a business partnership with UPS. You can also track your
-            order online.
+            Logistics.pk maintains several thousand locations within Pakistan
+            and worldwide as well as its own airline operations, delivery
+            vehicles and couriers. Logistics.pk has been assigned machine
+            readable passports (MRPs) delivery services by the government and
+            Visa application submission and delivery services by embassies.
+            Logistics.pk also maintains a business partnership with UPS. You can
+            also track your order online.
           </p>
         </Col>
       </Row>
